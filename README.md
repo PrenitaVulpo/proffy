73f059ad460aa98313b4c9a4b8a47b19bfd6f4f2
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-09-15 18:11:20 -0300

    Iniciando as rotas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proffy
 Plataforma de aulas online
+
+Reprodução da plataforma "proffy", mostrada no evento Next Level Week 2
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 
-import imgLogo from '../../assets/images/logo.svg';
-import imgLanding from '../../assets/images/landing.svg';
-import imgClasses from '../../assets/images/icons/study.svg';
-import imgStudy from '../../assets/images/icons/give-classes.svg';
-import imgPurpleHearth from '../../assets/images/icons/purple-heart.svg';
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './style.css'
 
 function Landing(){
-    return (    
-        <div id="landing-page">
-            <div id="landing-page-content" className="container">
-                <div id="logo-container">
-                    <img src={imgLogo} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online</h2>
-                </div>
-                <img src={imgLanding} alt="Plataforma de estudos"
-                className="hero-image"/>
-                <div className="buttons-container">
-                    <a href="" className="study">
-                        Estudar
-                        <img src={imgStudy} alt="Estudar"/>
-                    </a>
-                    <a href="" className="give-classes">
-                        Ensinar
-                        <img src={imgClasses} alt="Ensinar"/>
-                    </a>
-                </div>
-                <span className="total-connections">
-                    Já são X conexões realizadas
-                    <img src={imgPurpleHearth} alt="Coração roxo"/>
-                </span>
+    return (
+        <div id="page-landing">
+          <div id="page-landing-content" className="container">
+            <div className="logo-container">
+              <img src={logoImg} alt="Proffy" />
+              <h2>Sua plataforma de estudos online.</h2>
             </div>
+            <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
+    
+            <div className="buttons-container">
+              <a href="/study" className="study">
+                <img src={studyIcon} alt="Estudar" />
+                Estudar
+              </a>
+    
+              <a href="/give-classes" className="give-classes">
+                <img src={giveClassesIcon} alt="Ensinar" />
+                Ensinar
+              </a>
+            </div>
+    
+            <span className="total-connections">
+              Já são
+              X
+              conexões já realizadas
+              <img src={purpleHeartIcon} alt="Coração roxo" />
+            </span>
+          </div>
         </div>
-    )
+      );
 }
 
 export default Landing;
